6decc85352de21aea59564a48a7ca22c8d706901
Hugo <xwongtszhin@gmail.com>
2017-02-19 20:19:26 +0800

    add the Error Handling:statusCode = 403

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var _           = require('lodash');
 var TelegramBot = require('node-telegram-bot-api');
 var app         = express();
 
-var targetUrl = config.targetUrl;
+var targetUrl = 'https://www.biyroamer.com/category/adidas/ultra-boost';
 var token = config.telegram.token;
 var bot = new TelegramBot(token);
 var lastSend;
@@ -23,20 +23,25 @@ every('5s').do(function() {
             $('.entry-date.updated').first().filter(function(index, value){
                 data[index].datetime = value.attribs.datetime;
             });
+            if (lastSend != new Date(data[0].datetime).getTime()){
+                var chatID = []; 
+                bot.getUpdates().then(function(result){
+                    for(i in result){
+                        chatID = _.union(chatID, [result[i].message.chat.id]);
+                    }
+                    for( id in chatID){
+                        bot.sendMessage(chatID[id], data[0].word + "\n" + data[0].link + "\n" + data[0].datetime).then(function(resp){
+                            
+                        }).catch(function(error) {
+                            if (error.response && error.response.statusCode === 403) {
+                                 console.log('bot was blocked by the user');
+                        }});
+                    }
+                });
+                lastSend = new Date(data[0].datetime).getTime();
+            }
         }
     });
-    if (lastSend != new Date(data[0].datetime).getTime()){
-        var chatID = []; 
-        bot.getUpdates().then(function(result){
-            for(i in result){
-                chatID = _.union(chatID, [result[i].message.chat.id]);
-            }
-            for( id in chatID){
-                bot.sendMessage(chatID[id], data[0].word + "\n" + data[0].link + "\n" + data[0].datetime);
-            }
-        });
-        lastSend = new Date(data[0].datetime).getTime();
-    }
 });
 
 app.listen('8081');
