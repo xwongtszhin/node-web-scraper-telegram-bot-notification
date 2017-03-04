cf19a0fdaf56217bb6a96fe2235a22be4c0ba65a
Hugo Wong <xwongtszhin@users.noreply.github.com>
2017-03-05 02:06:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ var request     = require('request');
 var cheerio     = require('cheerio');
 var config      = require('./config.js');
 var TelegramBot = require('node-telegram-bot-api');
+var http        = require('http');
 var app         = express();
 
-var targetUrl = config.targetUrl;;
+var targetUrl = config.targetUrl;
 var token = config.telegram.token;
 var groupName = config.telegram.groupName;
 var bot = new TelegramBot(token);
 var lastSend;
+var time;
 
 app.get('/', function(req, res){
 	return res.status(200).json({lastSend: lastSend});
 });
-
-every('5s').do(function() {
+every('15s').do(function() {
     var data = [];
     request(targetUrl, function(error, response, html){
         if(!error){
@@ -27,16 +28,20 @@ every('5s').do(function() {
             $('.entry-date.updated').first().filter(function(index, value){
                 data[index].datetime = value.attribs.datetime;
             });
-            if (lastSend != new Date(data[0].datetime).getTime()){
-		bot.sendMessage(groupName, data[0].word + "\n" + data[0].link + "\n" + data[0].datetime).then(function(resp){
-		}).catch(function(error) {
-		    if (error.response && error.response.statusCode === 403) console.log('bot was blocked by the user');
-		});
-	    lastSend = new Date(data[0].datetime).getTime();
-            }
+            time = new Date(data[0].datetime).getTime();
+            if (lastSend != time)
+                telegramSend(data);
         }
     });
 });
+function telegramSend(data){
+    bot.sendMessage( groupName , data[0].word + "\n" + data[0].link + "\n" + data[0].datetime ).then( function(resp){
+        lastSend = time;
+    }).catch(function(error) {
+        if (error.response && error.response.statusCode === 403) 
+            console.log('bot was blocked by the user');
+    });
+};
 
 app.listen(process.env.PORT || 5000);
 exports = module.exports = app;
