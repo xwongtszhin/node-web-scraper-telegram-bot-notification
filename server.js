9787f246c579a4945f9bea2130676ac7e3988acf
Hugo <xwongtszhin@gmail.com>
2017-09-16 22:54:17 +0800

    HotFix:response is undefined

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var every       = require('schedule').every;
-var express     = require('express');
-var request     = require('request');
-var cheerio     = require('cheerio');
-var config      = require('./config.js');
+var every = require('schedule').every;
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var config = require('./config.js');
 var TelegramBot = require('node-telegram-bot-api');
-var http        = require('http');
-var app         = express();
+var http = require('http');
+var app = express();
 
 var targetUrl = config.targetUrl;
 var token = config.telegram.token;
@@ -14,31 +14,35 @@ var bot = new TelegramBot(token);
 var lastSend;
 var time;
 
-app.get('/', function(req, res){
-	return res.status(200).json({lastSend: lastSend});
+app.get('/', function (req, res) {
+    return res.status(200).json({ lastSend: lastSend });
 });
-every('15s').do(function() {
+every('15s').do(function () {
     var data = [];
-    request(targetUrl, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
-            $('.post-title.entry-title').first().filter(function(index, value){
-                data.push({"link": value.children[0].attribs.href, "word": value.children[0].children[0].data, "datetime": null});
-            });
-            $('.entry-date.updated').first().filter(function(index, value){
-                data[index].datetime = value.attribs.datetime;
-            });
-            time = new Date(data[0].datetime).getTime();
-            if (lastSend != time)
-                telegramSend(data);
+    request(targetUrl, function (error, response, html) {
+        if (!error) {
+            try {
+                var $ = cheerio.load(html);
+                $('.post-title.entry-title').first().filter(function (index, value) {
+                    data.push({ "link": value.children[0].attribs.href, "word": value.children[0].children[0].data, "datetime": null });
+                });
+                $('.entry-date.updated').first().filter(function (index, value) {
+                    data[index].datetime = value.attribs.datetime;
+                });
+                time = new Date(data[0].datetime).getTime();
+                if (lastSend != time)
+                    telegramSend(data);
+            } catch (err) {
+                console.log("ERROR");
+            }
         }
     });
 });
-function telegramSend(data){
-    bot.sendMessage( groupName , data[0].word + "\n" + data[0].link + "\n" + data[0].datetime ).then( function(resp){
+function telegramSend(data) {
+    bot.sendMessage(groupName, data[0].word + "\n" + data[0].link + "\n" + data[0].datetime).then(function (resp) {
         lastSend = time;
-    }).catch(function(error) {
-        if (error.response && error.response.statusCode === 403) 
+    }).catch(function (error) {
+        if (error.response && error.response.statusCode === 403)
             console.log('bot was blocked by the user');
     });
 };
