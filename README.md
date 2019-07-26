7dbf5e587d2158cc792bc0c1cf2efbb093ee566d
Hugo Wong <xwongtszhin@gmail.com>
2019-07-26 21:21:28 +0800

    feat: use the new package node-schedule

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# (Web Scraper) NodeJS with Telegram Bot Notification
-Simple web scraper to get the update of shoes information from biyroamer.
+# Telegram Bot Notification
+Simple Telegram Bot Notification
+Send Notification when the page create a new post
 
 Url:https://www.biyroamer.com
 
 ## Getting Started
-1. Install `nodejs`, `npm` and `yarn`
+1. Install `yarn`
 2. Run yarn install `yarn install`
 3. Copy .env_smaple to .env
 4. Add TELEGRAM_TOKEN and TELEGRAM_GROUP_NAME in .env file
-5. `npm start`
+5. `yarn start`
 
 ![123123](https://user-images.githubusercontent.com/10266845/44355407-21692000-a4df-11e8-8a1c-79b584da3fa3.png)
--- server.js
+++ server.js
@@ -1,52 +1,41 @@
-const every = require("schedule").every;
-const express = require("express");
-const request = require("request");
-const cheerio = require("cheerio");
-const TelegramBot = require("node-telegram-bot-api");
-require("dotenv").config();
-const app = express();
+/* eslint-disable no-multi-assign */
+const schedule = require('node-schedule');
+const express = require('express');
+const axios = require('axios');
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
 
+const app = express();
 const targetUrl = process.env.TARGET_URL;
 const token = process.env.TELEGRAM_TOKEN;
 const groupName = process.env.TELEGRAM_GROUP_NAME;
 const bot = new TelegramBot(token);
-let lastDateTime;
-let dateTime;
+let latestId = 0;
 
-every("20s").do(() => {
-  request(targetUrl, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      let $ = cheerio.load(html);
-      let data = {};
+schedule.scheduleJob('20 * * * * *', async () => {
+  try {
+    const response = await axios.get(`${targetUrl}/wp-json/wp/v2/posts`, {
+      params: {
+        per_page: 1,
+        context: 'embed',
+      },
+    });
 
-      $(".post-title.entry-title")
-        .first()
-        .filter((index, value) => {
-          data.url = value.children[0].attribs.href;
-          data.title = value.children[0].children[0].data;
+    if (response.status === 200) {
+      const {
+        date, title, link, id,
+      } = response.data[0];
+      if (id > latestId) {
+        bot.sendMessage(groupName, [title, link, date].join('\n')).then(() => {
+          latestId = id;
         });
-
-      $(".entry-date.updated")
-        .first()
-        .filter((index, value) => {
-          data.dateTime = value.attribs.datetime;
-        });
-
-      dateTime = new Date(data.dateTime).getTime();
-
-      if (lastDateTime != dateTime) sendTelegramMessage(data);
+      }
     }
-  });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
 });
-
-const sendTelegramMessage = data => {
-  bot
-    .sendMessage(groupName, [data.title, data.url, data.dateTime].join("\n"))
-    .then(resp => {
-      lastDateTime = dateTime;
-    })
-    .catch(error => {});
-};
 
 app.listen(process.env.PORT || 3000);
 exports = module.exports = app;
